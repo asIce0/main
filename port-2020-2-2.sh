fdd2c5ad0d1d8d7cc599e51df8209b5f32a99a9f
root <root@vohaw2tgcm.upkvxua.com>
2020-02-05 22:08:33 -0500

    my comment

---
+++ port-2020-2-2.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#echo $1
+
+port=
+
+echo "port is ${port}"
+
+config=/etc/sysconfig/iptables
+
+case "$1" in
+"open")
+echo "open"
+sed -i 's/\#-A INPUT -p tcp -m state --state NEW -m tcp --dport '"${port}"' -j ACCEPT/-A INPUT -p tcp -m state --state NEW -m tcp --dport '"${port}"' -j ACCEPT/' ${config}
+systemctl restart iptables
+;;
+"down")
+echo "down"
+sed -i 's/-A INPUT -p tcp -m state --state NEW -m tcp --dport '"${port}"' -j ACCEPT/\#-A INPUT -p tcp -m state --state NEW -m tcp --dport '"${port}"' -j ACCEPT/' ${config}
+systemctl restart iptables
+;;
+*)
+echo input open or down
+exit 0
+;;
+
+esac
+
+echo "##
+##
+##"
+
+iptables -n -L
